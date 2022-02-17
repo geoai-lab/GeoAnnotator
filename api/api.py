72860421668aec845531c78151e39a0c2e352772
Voltaire Vergara <42967921+jv11699@users.noreply.github.com>
2022-02-17 11:26:32 -0500

    Added NeuroTPR highlights and Nominatim Automatic Geodecoder; at the same time There are issues with said features; post request not yet enabled

--- api/api.py
+++ api/api.py
@@ -15,16 +15,26 @@ class tweet_database(db.Model):
     created_at = db.Column(db.String)
     def __str__(self):
         return f'{self.id},{self.text},{self.created_at}'
-def tweet_serializer(tweet):
-    return{
-        'id': tweet.id,
-        'content': tweet.content
-    }
+class tpr_database(db.Model):
+    __tablename__ = 'NeuroTPR-dataset'
+  
+    text = db.Column(db.String, nullable = False)
+    id = db.Column(db.Integer, primary_key = True)
+    created_at = db.Column(db.String)
+    neuro_data = db.Column(db.String, nullable = False)
+    def __str__(self):
+        return f'{self.id},{self.text},{self.created_at},{self.neuro_data}'
 
+import json
 @app.route('/api', methods=['GET'])
 def index():
-    tweets = tweet_database.query.order_by(func.random()).first()
+
+    tweets = tpr_database.query.order_by(func.random()).first()
     content = tweets.text
-    return jsonify(content)
+    neuro_results_json = json.loads(tweets.neuro_data)
+    toSend = {'id': tweets.id, 
+     'content': content,
+     'neuro_result':neuro_results_json}
+    return jsonify(toSend)
 if __name__ == '__main__':
     app.run(debug = True)
